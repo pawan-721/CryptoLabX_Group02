b61c05703dd184007b6704cb69e8910720033af4
2024ucp1766 <regarpawan721@gmail.com>
2026-08-05 18:25:43 +0000

    Add menu driven CLI

---
+++ main.py
@@ -0,0 +1,53 @@
+def encrypt():
+    print("Encrypt - Coming Soon")
+
+
+def decrypt():
+    print("Decrypt - Coming Soon")
+
+
+def attack():
+    print("Attack - Coming Soon")
+
+
+def analyze():
+    print("Analyze - Coming Soon")
+
+
+def show_menu():
+    print("\n===== CryptoLabX =====")
+    print("1. Encrypt")
+    print("2. Decrypt")
+    print("3. Attack")
+    print("4. Analyze")
+    print("5. Exit")
+
+
+def main():
+    while True:
+        show_menu()
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            encrypt()
+
+        elif choice == "2":
+            decrypt()
+
+        elif choice == "3":
+            attack()
+
+        elif choice == "4":
+            analyze()
+
+        elif choice == "5":
+            print("Thank you for using CryptoLabX.")
+            break
+
+        else:
+            print("Invalid choice. Try again.")
+
+
+if __name__ == "__main__":
+    main()
